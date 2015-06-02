59081127dfa4907dc7af497a9284aa22697e970f
kresnikwang <kresnikwang@gmail.com>
2015-06-02 01:15:29 -0700

    search completed

--- js/services/guide.js
+++ js/services/guide.js
@@ -3,7 +3,7 @@
  */
 
 app.factory('guides', ['$http', function($http) {
-  return $http.get('js/services/guide_name.json')
+  return $http.get('js/services/guide.json')
          .success(function(data) {
            return data;
          })
